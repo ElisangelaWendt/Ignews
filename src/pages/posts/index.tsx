38b079e9fade7d7240d8c861e764b3f59954835c
ElisangelaWendt <61567233+ElisangelaWendt@users.noreply.github.com>
2022-01-07 15:49:52 -0300

    Update index.tsx

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,11 @@
-import Head from "next/head";
-import { getPrismicClient } from "../../services/prismic";
-import styles from "./styles.module.scss";
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic'
+import styles from './styles.module.scss'
+
 import Prismic from '@prismicio/client'
-import { GetStaticProps } from "next";
 import { RichText } from 'prismic-dom'
+import Link from 'next/link'
 
 type Post = {
   slug: string;
@@ -12,60 +14,60 @@ type Post = {
   updatedAt: string;
 }
 
-interface PostProps{
+interface PostProps {
   posts: Post[]
 }
 
-export default function Posts({posts}: PostProps) {
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
-        <title>Posts | Ignews</title>
+        <title>Posts | ig.news</title>
       </Head>
+
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-          <a key={post.slug} href="#">
-            <time>{post.updatedAt}</time>
-            <strong>
-              {post.title}
-            </strong>
-            <p>
-              {post.excerpt}
-            </p>
-          </a> 
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
     </>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient()
+  const prismic = getPrismicClient();
 
-  const response = await prismic.query([
+  const response = await prismic.query([  
     Prismic.predicates.at('document.type', 'publication')
-  ],{
-    fetch: ['publication.title', 'publication.content'], //quais dados buscar do prismic
-    pageSize: 100,
+  ], {
+    fetch: ['title', 'content'],
+    pageSize: 100
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
   })
 
-  // const posts = response.results.map(post => {
-  //   return{
-  //     slug: post.uid,
-  //     title: RichText.asText(post.data.title),
-  //     excerpt: post.data.content.find(content => content.type === 'paragraph') ?.text ?? '',
-  //     updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
-  //       day: '2-digit',
-  //       month: 'long',
-  //       year: 'numeric'
-  //     }
-  //     )
-  //   }
-  // })
-
-  return{ 
-    props: {}
+  return {
+    props: {
+      posts
+    }
   }
 }
